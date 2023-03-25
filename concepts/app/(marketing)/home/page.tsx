9c01e834313b42cf8077b5652ac973ca0e125b7d
ROSHAN <roshan@MACBOOKs-MacBook-Pro.local>
2023-03-25 18:32:47 +0530

    Route Grouping

---
+++ concepts/app/(marketing)/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Route grouping => adding list of related routes under one folder, naming rule => folder name within parenthesis ()
+function Home() {
+  return (
+    <div>Home using Route Grouping</div>
+  )
+}
+
+export default Home
+
