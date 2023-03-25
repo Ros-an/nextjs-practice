54322f8ea75b236e6e838b661cb3b99401eb14a9
ROSHAN <roshan@MACBOOKs-MacBook-Pro.local>
2023-03-25 18:42:47 +0530

    route for our app

--- concepts/app/about/me/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Me() {
-    return <section>
-        <h1>This is Me page</h1>
-    </section>
-}
--- concepts/app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function About() {
-    return <section>
-        <h1>This is about page</h1>
-    </section>
-}
