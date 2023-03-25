d0609a0badf3956b6a83cc46f6574c92d6d03626
ROSHAN <roshan@MACBOOKs-MacBook-Pro.local>
2023-03-25 18:09:08 +0530

    dynamic nested route

---
+++ concepts/app/posts/[...slug]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// baseUrl/posts/123/agb/9Kdmd/... => for something like this, basically when you want to handle nested route
+function MatchRouteAfterSlug() {
+  return (
+    <div>MatchRouteAfterSlug</div>
+  )
+}
+
+export default MatchRouteAfterSlug
