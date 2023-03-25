86dbb21789bd307cdc82715b2d54033051927172
ROSHAN <roshan@MACBOOKs-MacBook-Pro.local>
2023-03-25 18:37:07 +0530

    Route Grouping collision

--- concepts/app/(marketing)/home/page.tsx
+++ concepts/app/(marketing)/home/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 // Route grouping => adding list of related routes under one folder, naming rule => folder name within parenthesis ()
+
+// Now one question arises, what if there is collision in naming
+/**
+ * like
+ * (marketing)/home
+ * home
+ * In this case the content of first one will be rendered, First Come First Serve
+ * 
+ */
 function Home() {
   return (
     <div>Home using Route Grouping</div>
